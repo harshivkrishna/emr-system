2188a08f1291625a5dbf2a050fe4ca4688eb2423
harshivkrishna <harshivkrishnam.cse2023@citchennai.net>
2025-06-15 20:50:07 +0530

    renamed:    backend/index.js -> backend/api/index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,15 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import patientRoutes from './routes/patients.js';
-import prescriptionRoutes from './routes/prescriptions.js';
-import followUpRoutes from './routes/followups.js';
-import searchRoutes from './routes/search.js';
-
+import patientRoutes from '../routes/patients.js';
+import prescriptionRoutes from '../routes/prescriptions.js';
+import followUpRoutes from '../routes/followups.js';
+import searchRoutes from '../routes/search.js';
+import serverless from 'serverless-http';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -56,6 +56,5 @@ app.use('*', (req, res) => {
   res.status(404).json({ error: 'Route not found' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+export default serverless(app);
